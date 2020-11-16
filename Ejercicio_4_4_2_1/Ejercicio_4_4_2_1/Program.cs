e76e6343f16fd9bedb6271d7744cfe6d04e66a14
FerTV <fernando.torres.vega@gmail.com>
2020-11-16 10:57:08 +0100

    Añadido Ejercicio_4_4_2_1

---
+++ Ejercicio_4_4_2_1/Ejercicio_4_4_2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio_4_4_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Introduce tu nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("La inicial de tu nombre es {0}", nombre[0]);
+        }
+    }
+}
