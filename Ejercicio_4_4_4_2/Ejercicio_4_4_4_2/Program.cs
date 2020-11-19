67ef423adc6bf41e07b4c063eacbea3fec028425
FerTV <fernando.torres.vega@gmail.com>
2020-11-19 10:50:16 +0100

    Ejercicio_4_4_4_2

---
+++ Ejercicio_4_4_4_2/Ejercicio_4_4_4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_4_4_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string subcadena;
+
+            Console.Write("Introduce tu nombre: ");
+            nombre = Console.ReadLine();
+
+            for(int j=1;j<=nombre.Length;j++)
+            {
+                subcadena = nombre.Substring(0, j);
+                Console.Write("{0}\n", subcadena);
+            }
+        }
+    }
+}
