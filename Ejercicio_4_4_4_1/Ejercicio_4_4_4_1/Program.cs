2324ad3efa16454982538d0ecd2ff3b6fa0f4324
FerTV <fernando.torres.vega@gmail.com>
2020-11-19 11:03:11 +0100

    Ejercicio_4_4_5_1 añadido

--- Ejercicio_4_4_4_1/Ejercicio_4_4_4_1/Program.cs
+++ Ejercicio_4_4_4_1/Ejercicio_4_4_4_1/Program.cs
@@ -8,15 +8,16 @@ namespace Ejercicio_4_4_4_1
         {
             string nombre;
             string subcadena;
-            int j = 0;
+            int i = 1;
 
             Console.Write("Introduce el nombre: ");
             nombre = Console.ReadLine();
 
-            for (int i = 0; i < nombre.Length; i++)
+            for (int j = 0; i <= nombre.Length; j++)
             {
-                subcadena = nombre.Substring(i, i+1);
-                Console.Write("{0}", subcadena);
+                subcadena = nombre.Substring(i, j);
+                Console.Write("{0} ", subcadena);
+                i++;
             }
         }
     }
---
+++ Ejercicio_4_4_5_1/Ejercicio_4_4_5_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_4_4_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUM_FRASES = 10;
+            string[] frase = new string[NUM_FRASES];
+            string texto_buscar;
+
+            for(int i=0;i<NUM_FRASES;i++)
+            {
+                Console.Write("Introduce la frase {0}: ", i + 1);
+                frase[i] = Console.ReadLine();
+            }
+
+            do
+            {
+                Console.Write("Introduce el texto a buscar: ");
+                texto_buscar = Console.ReadLine();
+
+                for(int i=0;i<NUM_FRASES;i++)
+                {
+                    if (frase[i].Contains(texto_buscar))
+                        Console.WriteLine("El texto a buscar aparece en la frase {0}", i + 1);
+                }
+            } while (texto_buscar != "fin");
+        }
+    }
+}
