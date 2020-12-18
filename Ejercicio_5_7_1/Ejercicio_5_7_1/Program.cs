c91dc992acbe5f697bd6e8e4c72dd4047a91504a
FerTV <fernando.torres.vega@gmail.com>
2020-12-18 13:40:23 +0100

    Ejercicios 5_7

---
+++ Ejercicio_5_7_1/Ejercicio_5_7_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_5_7_1
+{
+    class Program
+    {
+        public static void Intercambiar(ref int n1, ref int n2)
+        {
+            int temporal=n1;
+
+            n1 = n2;
+            n2 = temporal;
+        }
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.Write("Introduce el número n1: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduce el número n2: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Intercambiar(ref n1, ref n2);
+            Console.WriteLine("Ahora n1 es {0}, y n2 es {1}", n1, n2);
+        }
+    }
+}
---
+++ Ejercicio_5_7_2/Ejercicio_5_7_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_5_7_2
+{
+    class Program
+    {
+        public static void Iniciales(string cadena, out char letra1, out char letra2)
+        {
+            char delimitador = ' ';
+            string[] frase;
+
+            frase = cadena.Split(delimitador);
+
+            letra1 = frase[0][0];
+            letra2 = frase[1][0];
+        }
+        static void Main(string[] args)
+        {
+            char letra1, letra2;
+            string cadena;
+
+            Console.Write("Introduce una cadena: ");
+            cadena = Console.ReadLine();
+            Iniciales(cadena,out letra1, out letra2);
+            Console.WriteLine("La primera letra es {0} y la siguiente letra es {1}", letra1, letra2);
+        }
+    }
+}
