8e9ba75bdc6d687f346e80aa822af361698c1920
FerTV <fernando.torres.vega@gmail.com>
2021-01-11 09:56:13 +0100

    Terminado 9_5

--- Ejercicio_5_9_1_5/Ejercicio_5_9_1_5/Program.cs
+++ Ejercicio_5_9_1_5/Ejercicio_5_9_1_5/Program.cs
@@ -6,20 +6,27 @@ namespace Ejercicio_5_9_1_5
     {
         static void Main(string[] args)
         {
-            int[,] pantalla = new int[24,79];
+            int[,] pantalla = new int[24, 79];
             Random r = new Random();
             int alto_random, ancho_random;
 
-            for(int i=0;i<100;i++)
+            for (int i = 0; i < 100; i++)
             {
                 alto_random = r.Next(0, 24);
                 ancho_random = r.Next(0, 79);
+
+                do
+                {
+                    alto_random = r.Next(0, 24);
+                    ancho_random = r.Next(0, 79);
+                } while (pantalla[alto_random, ancho_random] == '*');
+
                 pantalla[alto_random, ancho_random] = '*';
             }
 
-            for(int i=0;i<24;i++)
+            for (int i = 0; i < 24; i++)
             {
-                for(int j=0;j<79;j++)
+                for (int j = 0; j < 79; j++)
                 {
                     if (pantalla[i, j] == '*')
                         Console.Write("*");
