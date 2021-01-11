541ddd9c624a46b8fcae7bd135cd269d6e9151b5
FerTV <fernando.torres.vega@gmail.com>
2021-01-11 09:48:45 +0100

    Terminado 5_9

---
+++ Ejercicio_5_9_1_4/Ejercicio_5_9_1_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio_5_9_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[100];
+            Random r = new Random();
+            int aleatorio, media, suma = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                aleatorio = r.Next(-1000, 1001);
+                numeros[i] = aleatorio;
+                suma += numeros[i];
+            }
+            media = suma / 100;
+
+            Console.WriteLine("La media de los números aleatorios es: {0}", media);
+        }
+    }
+}
---
+++ Ejercicio_5_9_1_5/Ejercicio_5_9_1_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio_5_9_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] pantalla = new int[24,79];
+            Random r = new Random();
+            int alto_random, ancho_random;
+
+            for(int i=0;i<100;i++)
+            {
+                alto_random = r.Next(0, 24);
+                ancho_random = r.Next(0, 79);
+                pantalla[alto_random, ancho_random] = '*';
+            }
+
+            for(int i=0;i<24;i++)
+            {
+                for(int j=0;j<79;j++)
+                {
+                    if (pantalla[i, j] == '*')
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
