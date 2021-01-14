4bc1124b7f9de081ab58b6050bf72e039d0c70c6
FerTV <fernando.torres.vega@gmail.com>
2021-01-14 10:38:56 +0100

    5_9_2_8

---
+++ Ejercicio_5_9_2_7/Ejercicio_5_9_2_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio_5_9_2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double angulo;
+
+            Console.Write("Introduce un ángulo: ");
+            angulo = Convert.ToDouble(Console.ReadLine());
+
+            angulo = angulo*(Math.PI/180);
+            Console.WriteLine("El coseno del ángulo es: " + Math.Cos(angulo));
+            Console.WriteLine("El seno del ángulo es: " + Math.Sin(angulo));
+            Console.WriteLine("La tangente del ángulo es: " + Math.Tan(angulo));
+        }
+    }
+}
---
+++ Ejercicio_5_9_2_8/Ejercicio_5_9_2_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_5_9_2_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] valores = new double[73];
+
+            for (int i = 0; i < 73; i++)
+            {
+                valores[i] = 10 * Math.Sin(5 * i * (Math.PI / 180));
+            }
+            for (int i = 0; i < 73; i++)
+            {
+                Console.WriteLine("Valor de la función para {0} grados: {1}", i, valores[i]);
+            }
+        }
+    }
+}
