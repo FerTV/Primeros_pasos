e7674ae4120535253f540fcb89a1f020991b941b
FerTV <fernando.torres.vega@gmail.com>
2021-01-15 13:22:49 +0100

    Corregido redondeo

---
+++ Ejercicio_5_10_1/Ejercicio_5_10_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_5_10_1
+{
+    class Program
+    {
+        public static int Potencia(int n, int potencia)
+        {
+            if (potencia == 1)
+                return n;
+            else
+                return n * Potencia(n, potencia - 1);
+        }
+        static void Main(string[] args)
+        {
+            int n, potencia, resultado;
+
+            Console.Write("Introduce un número: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduce una potencia: ");
+            potencia = Convert.ToInt32(Console.ReadLine());
+
+            resultado = Potencia(n, potencia);
+
+            Console.WriteLine("El resultado de {0} elevado a {1} es: {2}", n, potencia, resultado);
+        }
+    }
+}
--- Ejercicio_5_9_2_7/Ejercicio_5_9_2_7/Program.cs
+++ Ejercicio_5_9_2_7/Ejercicio_5_9_2_7/Program.cs
@@ -12,9 +12,9 @@ namespace Ejercicio_5_9_2_7
             angulo = Convert.ToDouble(Console.ReadLine());
 
             angulo = angulo*(Math.PI/180);
-            Console.WriteLine("El coseno del ángulo es: " + Math.Cos(angulo));
-            Console.WriteLine("El seno del ángulo es: " + Math.Sin(angulo));
-            Console.WriteLine("La tangente del ángulo es: " + Math.Tan(angulo));
+            Console.WriteLine("El coseno del ángulo es: " + Math.Round( Math.Cos(angulo)));
+            Console.WriteLine("El seno del ángulo es: " + Math.Round(Math.Sin(angulo)));
+            Console.WriteLine("La tangente del ángulo es: " + Math.Round(Math.Tan(angulo)));
         }
     }
 }
