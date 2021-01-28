5508079c90e4d3533dec3b84136408219320aee5
FerTV <fernando.torres.vega@gmail.com>
2021-01-28 08:46:29 +0100

    5_10_2

---
+++ Ejercicio_5_10_2/Ejercicio_5_10_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_5_10_2
+{
+    class Program
+    {
+        public static int Potencia(int n, int potencia)
+        {
+            int resultado = 0; 
+
+            for(int i=1;i<potencia;i++)
+            {
+                if (i == 1)
+                    resultado = resultado + (n * n);
+                else
+                    resultado = resultado * n;
+            }
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            int numero, potencia, resultado;
+
+            Console.Write("Introduce un número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduce una potencia: ");
+            potencia = Convert.ToInt32(Console.ReadLine());
+
+            resultado = Potencia(numero, potencia);
+
+            Console.WriteLine("El resultado es: {0}", resultado);
+        }
+    }
+}
---
+++ Ejercicio_5_10_3/Ejercicio_5_10_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_5_10_3
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 0)
+                return n;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
