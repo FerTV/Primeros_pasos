35a76b7052e224be2e8dbb3463c02bc1709f0085
FerTV <fernando.torres.vega@gmail.com>
2021-02-01 13:24:59 +0100

    Ejercicios argumentos

---
+++ Ejercicio_5_11_1/Ejercicio_5_11_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_5_11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma, n1, n2;
+
+            if(args.Length<=0)
+            {
+                Console.WriteLine("No hay suficientes datos.");
+            }
+            else if(args.Length<2)
+            {
+                Console.WriteLine(args[0]);
+            }
+            else if(args.Length>=2)
+            {
+                n1 = Convert.ToInt32(args[0]);
+                n2 = Convert.ToInt32(args[1]);
+                suma = n1 + n2;
+                Console.WriteLine(suma);
+            }
+        }
+    }
+}
---
+++ Ejercicio_5_11_2/Ejercicio_5_11_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio_5_11_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = 0, n1, n2;
+
+            if(args.Length>=3)
+            {
+                n1 = Convert.ToInt32(args[0]);
+                n2 = Convert.ToInt32(args[2]);
+
+                switch(args[1])
+                {
+                    case "+":
+                        resultado = n1 + n2;
+                        break;
+                    case "-":
+                        resultado = n1 - n2;
+                        break;
+                    case "*":
+                        resultado = n1 * n2;
+                        break;
+                    case "/":
+                        if (args[2] != "0")
+                        {
+                            resultado = n1 / n2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede dividir entre 0");
+                        }
+                            break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+
+                Console.WriteLine("El resultado es: {0}", resultado);
+            }
+            else
+            {
+                Console.WriteLine("Error no hay suficientes argumentos");
+            }
+        }
+    }
+}
