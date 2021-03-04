f73e829105ef891c23a9df928a94e87722365a7d
FerTV <fernando.torres.vega@gmail.com>
2021-03-04 11:12:28 +0100

    Ejercicio_6_3_7

---
+++ POO/Ejercicio_6_3_7/Ejercicio_6_3_7/CCoche.cs
@@ -0,0 +1,43 @@
+﻿namespace Ejercicio_6_3_7
+{
+    class CCoche
+    {
+        private string marca;
+        private string modelo;
+        private int cilindrada;
+        private float potencia;
+
+        public void SetMarca(string marca)
+        {
+            this.marca = marca;
+        }
+        public void SetModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+        public void SetCilindrada(int cilindrada)
+        {
+            this.cilindrada = cilindrada;
+        }
+        public void SetPotencia(float potencia)
+        {
+            this.potencia = potencia;
+        }   
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public string GetModelo()
+        {
+            return this.modelo;
+        }
+        public int GetCilindrada()
+        {
+            return this.cilindrada;
+        }
+        public float GetPotencia()
+        {
+            return this.potencia;
+        }
+    }
+}
---
+++ POO/Ejercicio_6_3_7/Ejercicio_6_3_7/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_6_3_7
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CCoche coche;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            coche = new CCoche();
+        }
+
+        private void tb_introducir_datos_Click(object sender, RoutedEventArgs e)
+        {
+            coche.SetCilindrada(Convert.ToInt32(txt_cilindrada.Text));
+            coche.SetMarca(txt_marca.Text);
+            coche.SetModelo(txt_modelo.Text);
+            coche.SetPotencia(Convert.ToInt32(txt_potencia));
+        }
+
+        private void tb_mostrar_datos_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Los datos son: \n" +
+                            "Marca: " + coche.GetMarca() + "\n" +
+                            "Modelo: " + coche.GetModelo() + "\n" +
+                            "Cilindrada: " + coche.GetCilindrada() + "\n" +
+                            "Potencia: " + coche.GetPotencia() + "\n");
+        }
+    }
+}
