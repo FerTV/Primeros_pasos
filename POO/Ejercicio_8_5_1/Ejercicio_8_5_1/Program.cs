ddb62cbd109943b288945095a7a87d50723ce46f
FerTV <fernando.torres.vega@gmail.com>
2021-04-27 12:32:33 +0200

    c

--- POO/Ejercicio_8_5_1/Ejercicio_8_5_1/Program.cs
+++ POO/Ejercicio_8_5_1/Ejercicio_8_5_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Ejercicio_8_5_1
 {
@@ -6,7 +7,18 @@ namespace Ejercicio_8_5_1
     {
         static void Main(string[] args)
         {
-             
+             using(StreamWriter fichero = new StreamWriter(".\\resultados\\sumas.txt", true))
+             {
+                int n1, n2, suma;
+
+                Console.Write("Introduce el n1: ");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Introduce el n2: ");
+                n2 = Convert.ToInt32(Console.ReadLine());
+                suma = n1 + n2;
+                Console.WriteLine("{0} + {1} = {2}", n1, n2, suma);
+                fichero.WriteLine("{0} + {1} = {2}", n1, n2, suma);
+            }
         }
     }
 }
