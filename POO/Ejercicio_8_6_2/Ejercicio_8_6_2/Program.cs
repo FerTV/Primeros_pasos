8d77aed07cee8545c5ecb03393b172b9649a8baa
FerTV <fernando.torres.vega@gmail.com>
2021-04-29 10:38:54 +0200

    c

---
+++ POO/Ejercicio_8_6_2/Ejercicio_8_6_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+public class Ejercicio_8_6_2
+{
+    public static void Main()
+    {
+        StreamReader fichero;
+        string nombre;
+        bool parar=false;
+
+        while (parar) // Interrumpimos desde dentro con "break"
+        {
+            Console.Write("Nombre del fichero (\"fin\" para terminar)? ");
+            nombre = Console.ReadLine();
+            if (nombre == "fin")
+            {
+                parar = true;
+                break;
+            }
+            if (File.Exists(nombre))
+            {
+                fichero = File.OpenText(nombre);
+                Console.WriteLine("Su primera linea es: {0}",
+                fichero.ReadLine());
+                fichero.Close();
+            }
+            else
+                Console.WriteLine("No existe!");
+        }
+    }
+}
---
+++ POO/binario/binario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string extension = "";
+
+            Console.Write("Introduce el nombre del fichero: ");
+            nombre = Console.ReadLine();
+            FileStream fichero = File.OpenRead(nombre);
+
+            for (int pos = 0; pos < 10; pos++)
+            {
+                extension = extension + Convert.ToChar((byte)fichero.ReadByte());
+            }
+            Console.WriteLine("Los 50 primeros bytes son {0}", extension);
+            fichero.Close();
+        }
+    }
+}
