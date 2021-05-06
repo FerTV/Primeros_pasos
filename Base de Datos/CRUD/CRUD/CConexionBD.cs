590475e5230be63b1d927cb7f3246e46a4deba1f
FerTV <fernando.torres.vega@gmail.com>
2021-05-06 10:42:26 +0200

    c

---
+++ Base de Datos/CRUD/CRUD/CConexionBD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CRUD
+{
+    public class CConexionBD
+    {
+        // Cadena de conexión con la base de datos.
+        static private string cadenaConexion = @"Server=FTV-PC\SQLEXPRESS;DataBase=ERP;Integrated Security=true;";
+        // Conexión a la base de datos.
+        public SqlConnection Connection { get; } = new SqlConnection(cadenaConexion);
+        public void Abrir()
+        {
+            // Si la conexión está cerrada, la abrimos.
+            if (Connection.State == ConnectionState.Closed)
+                Connection.Open();
+        }
+
+        public void Cerrar()
+        {
+            // Si la conexión está abierta, la cerramos.
+            if (Connection.State == ConnectionState.Open)
+                Connection.Close();
+        }
+    }
+}
